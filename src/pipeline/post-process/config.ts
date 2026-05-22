cac204dae6bd3f2f962fcfa6808988fb4ac5c186
atolix <82761106+atolix@users.noreply.github.com>
2026-05-22 22:03:21 +0900

    Revert "Add glitch post-process effect"
    This reverts commit 8eb7a8bd3fc619e01440aa40a2a0c146698a53b4.

--- src/pipeline/post-process/config.ts
+++ src/pipeline/post-process/config.ts
@@ -1,5 +1,4 @@
 import chromaticAberrationSource from "../../shaders/effects/chromatic-aberration.frag?raw";
-import glitchSource from "../../shaders/effects/glitch.frag?raw";
 import invertSource from "../../shaders/effects/invert.frag?raw";
 import noiseSource from "../../shaders/effects/noise.frag?raw";
 import pixelateSource from "../../shaders/effects/pixelate.frag?raw";
@@ -64,13 +63,6 @@ export function createPostProcessPasses(
       includeResolver,
       false
     ),
-    createPostProcessPass(
-      "glitch",
-      glitchSource,
-      "./shaders/effects/glitch.frag",
-      includeResolver,
-      false
-    ),
   ];
 }
 
@@ -86,7 +78,6 @@ export function createPostProcessIncludeRoots(): ShaderIncludeRoot[] {
     { path: "./shaders/effects/pixelate.frag", source: pixelateSource },
     { path: "./shaders/effects/scanlines.frag", source: scanlinesSource },
     { path: "./shaders/effects/posterize.frag", source: posterizeSource },
-    { path: "./shaders/effects/glitch.frag", source: glitchSource },
   ];
 }
 
