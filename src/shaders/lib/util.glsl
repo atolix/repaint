debec9b45121fb5f37500437b2bdddeaebdcf699
atolix <82761106+atolix@users.noreply.github.com>
2026-06-13 23:04:36 +0900

    Add GLSL utility helpers

--- src/shaders/lib/util.glsl
+++ src/shaders/lib/util.glsl
@@ -1,3 +1,6 @@
+const float PI = 3.14159265359;
+const float TAU = 6.28318530718;
+
 float saturate(float value) {
   return clamp(value, 0.0, 1.0);
 }
@@ -19,6 +22,20 @@ float remap01(float value, float inMin, float inMax) {
   return saturate((value - inMin) / (inMax - inMin));
 }
 
+float remapClamped(float value, float inMin, float inMax, float outMin, float outMax) {
+  return mix(outMin, outMax, remap01(value, inMin, inMax));
+}
+
+vec2 safeNormalize(vec2 value) {
+  float len = length(value);
+  return len > 0.00001 ? value / len : vec2(0.0);
+}
+
+vec3 safeNormalize(vec3 value) {
+  float len = length(value);
+  return len > 0.00001 ? value / len : vec3(0.0);
+}
+
 vec2 rotate2D(vec2 p, float angle) {
   float s = sin(angle);
   float c = cos(angle);
@@ -27,6 +44,14 @@ vec2 rotate2D(vec2 p, float angle) {
 
 vec2 getScreenUV() {
   return gl_FragCoord.xy / u_resolution;
+}
+
+vec2 getPixelSize() {
+  return 1.0 / u_resolution;
+}
+
+vec2 getPixelUV() {
+  return (floor(gl_FragCoord.xy) + 0.5) / u_resolution;
 }
 
 vec2 getCenteredUV() {
@@ -40,6 +65,20 @@ vec2 aspectCorrect(vec2 uv) {
 
 vec2 getUV() {
   return aspectCorrect(getCenteredUV());
+}
+
+vec2 toPolar(vec2 p) {
+  return vec2(length(p), atan(p.y, p.x));
+}
+
+vec2 fromPolar(float radius, float angle) {
+  return vec2(cos(angle), sin(angle)) * radius;
+}
+
+vec2 barrelDistort(vec2 uv, float amount) {
+  vec2 centered = uv - 0.5;
+  float radius2 = dot(centered, centered);
+  return 0.5 + centered * (1.0 + amount * radius2);
 }
 
 float circleMask(vec2 uv, float radius, float softness) {
